fff5fbeeb9401310685f92969643b8c114ec56de
Наранхүү <codesaur@gmail.com>
2023-02-02 04:07:41 +0800

    rm

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,21 +1,21 @@
 <?php
 
-if (!function_exists('codesaur_error_log')) {
+if (!\function_exists('codesaur_error_log')) {
     function codesaur_error_log($errno, $errstr, $errfile, $errline)
     {
         switch ($errno) {
             case \E_USER_ERROR:
-                error_log("Error: $errstr \n Fatal error on line $errline in file $errfile \n");
+                \error_log("Error: $errstr \n Fatal error on line $errline in file $errfile \n");
                 break;
             case \E_USER_WARNING:
-                error_log("Warning: $errstr \n in $errfile on line $errline \n");
+                \error_log("Warning: $errstr \n in $errfile on line $errline \n");
                 break;
             case \E_USER_NOTICE:
-                error_log("Notice: $errstr \n in $errfile on line $errline \n");
+                \error_log("Notice: $errstr \n in $errfile on line $errline \n");
                 break;
             default:
                 if ($errno != 2048) {
-                    error_log("#$errno: $errstr \n in $errfile on line $errline \n");
+                    \error_log("#$errno: $errstr \n in $errfile on line $errline \n");
                 }
                 break;
         }
@@ -24,20 +24,20 @@ if (!function_exists('codesaur_error_log')) {
     }
 }
 
-if (!function_exists('codesaur_bootstrap')) {
+if (!\function_exists('codesaur_bootstrap')) {
     function codesaur_bootstrap()
     {
-        error_reporting(\E_ALL);
+        \error_reporting(\E_ALL);
 
-        $this_dir = dirname(__FILE__);
+        $this_dir = \dirname(__FILE__);
         $autoload = "$this_dir/../vendor/autoload.php";
-        if (!file_exists($autoload)) {
+        if (!\file_exists($autoload)) {
             die("codesaur exit: <strong>$autoload is missing!</strong>");
         }
         $composer = require($autoload);
 
-        ini_set('log_errors', 'On');
-        ini_set('error_log', "$this_dir/../logs/code.log");
+        \ini_set('log_errors', 'On');
+        \ini_set('error_log', "$this_dir/../logs/code.log");
 
         try {
             $dotenv = Dotenv\Dotenv::createImmutable("$this_dir/..");
@@ -53,12 +53,12 @@ if (!function_exists('codesaur_bootstrap')) {
             die("codesaur exit: <strong>{$e->getMessage()}</strong>");
         }
 
-        define('CODESAUR_DEVELOPMENT', isset($_ENV['CODESAUR_APP_ENV']) ? $_ENV['CODESAUR_APP_ENV'] != 'production' : false);
-        ini_set('display_errors', CODESAUR_DEVELOPMENT ? 'On' : 'Off');
-        set_error_handler('codesaur_error_log');
+        \define('CODESAUR_DEVELOPMENT', isset($_ENV['CODESAUR_APP_ENV']) ? $_ENV['CODESAUR_APP_ENV'] != 'production' : false);
+        \ini_set('display_errors', CODESAUR_DEVELOPMENT ? 'On' : 'Off');
+        \set_error_handler('codesaur_error_log');
 
         if (!empty($_ENV['CODESAUR_TIME_ZONE'])) {
-            date_default_timezone_set($_ENV['CODESAUR_TIME_ZONE']);
+            \date_default_timezone_set($_ENV['CODESAUR_TIME_ZONE']);
         }
 
         return $composer;
--- public_html/index.php
+++ public_html/index.php
@@ -5,18 +5,18 @@ use codesaur\Http\Message\ServerRequest;
 require_once '../application/bootstrap.php';
 
 $request = (new ServerRequest())->initFromGlobal();
-$uri_path = rawurldecode($request->getUri()->getPath());
-$script_path = dirname($request->getServerParams()['SCRIPT_NAME']);
+$uri_path = \rawurldecode($request->getUri()->getPath());
+$script_path = \dirname($request->getServerParams()['SCRIPT_NAME']);
 if ($script_path == '\\' || $script_path == '/') {
     $script_path = '';
 }
 if (!empty($script_path)) {
-    $uri_path = substr($uri_path, strlen($script_path));
+    $uri_path = \substr($uri_path, \strlen($script_path));
 }
 if (empty($uri_path)) {
     $uri_path = '/';
 }
-$pipe = explode('/', $uri_path)[1];
+$pipe = \explode('/', $uri_path)[1];
 if ($pipe == 'indo') {
     $script_path .= "/$pipe";    
     $application = new Indo\Application();
